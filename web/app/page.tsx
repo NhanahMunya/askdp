50a7578b64d5f04d94ebc369b478369535966a85
Munyaradzi Nhaitai <munynhait@gmail.com>
2026-06-02 16:06:08 +0200

    Fix: pie chart percent possibly undefined type error

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -423,7 +423,7 @@ function SmartChart({
             cy="50%"
             outerRadius={90}
             label={({ name, percent }) =>
-              `${name} ${(percent * 100).toFixed(0)}%`
+              `${name} ${((percent ?? 0) * 100).toFixed(0)}%`
             }
             labelLine
           >
